107dcdaa89ff59c44509e25046e9c9021248d778
User <User@LAB3-3>
2024-10-07 15:11:16 +0300

    Add project files.

---
+++ CutNowApp/Services/CutNowWebAPIProxy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+//using CutNowApp.Models;
+
+namespace CutNowApp.Services
+{
+    public class CutNowWebAPIProxy
+    {
+        #region without tunnel
+        /*
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "localhost";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110/api/" : $"http://{serverIP}:5110/api/";
+        private static string ImageBaseAddress = (DeviceInfo.Platform == DevicePlatform.Android &&
+            DeviceInfo.DeviceType == DeviceType.Virtual) ? "http://10.0.2.2:5110" : $"http://{serverIP}:5110";
+        */
+        #endregion
+
+        #region with tunnel
+        //Define the serevr IP address! (should be realIP address if you are using a device that is not running on the same machine as the server)
+        private static string serverIP = "sq41589w-7014.euw.devtunnels.ms";
+        private HttpClient client;
+        private string baseUrl;
+        public static string BaseAddress = "sq41589w-7014.euw.devtunnels.ms";
+        private static string ImageBaseAddress = "sq41589w-7014.euw.devtunnels.ms";
+        #endregion
+
+        public CutNowWebAPIProxy()
+        {
+            //Set client handler to support cookies!!
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new System.Net.CookieContainer();
+
+            this.client = new HttpClient(handler);
+            this.baseUrl = BaseAddress;
+        }
+
+        public string GetImagesBaseAddress()
+        {
+            return CutNowWebAPIProxy.ImageBaseAddress;
+        }
+
+        public string GetDefaultProfilePhotoUrl()
+        {
+            return $"{CutNowWebAPIProxy.ImageBaseAddress}/profileImages/default.png";
+        }
+    }
+}
